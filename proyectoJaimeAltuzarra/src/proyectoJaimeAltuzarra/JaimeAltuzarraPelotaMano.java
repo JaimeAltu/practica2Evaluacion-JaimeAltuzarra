be6d4e0ed2c8920104b75ee0816259ab0862c9ad
jaime <jaime@LAPTOT-JAIME02>
2022-02-16 22:08:05 +0100

    Agregando-ClaseJava

---
+++ proyectoJaimeAltuzarra/src/proyectoJaimeAltuzarra/JaimeAltuzarraPelotaMano.java
@@ -0,0 +1,14 @@
+package proyectoJaimeAltuzarra;
+
+public class JaimeAltuzarraPelotaMano {
+	public String categoriaPelota;
+	public int numerosFronton;
+	public String pelotaMano;
+	
+	public JaimeAltuzarraPelotaMano(String categoriaPelota, int numerosFronton, String pelotaMano) {
+		super();
+		this.categoriaPelota = categoriaPelota;
+		this.numerosFronton = numerosFronton;
+		this.pelotaMano = pelotaMano;
+	}
+}
